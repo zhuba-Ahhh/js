d05b11dbe61cfac5f33b32c033385de6cb0e0175
Yan-Wen-Jun <2926887977@qq.com>
2023-10-29 23:41:11 +0800

    feat: :sparkles: add ts

---
+++ utils/index.js
@@ -0,0 +1,16 @@
+function isSpreadable(obj) {
+  return (
+    typeof obj === "object" &&
+    obj !== null &&
+    typeof obj[Symbol.iterator] === "function"
+  );
+}
+
+const arr = [1, 2, 3];
+const obj = { a: 1, b: 2 };
+const str = "hello";
+console.log({...obj});
+
+console.log(isSpreadable(arr)); // true
+console.log(isSpreadable(obj)); // false
+console.log(isSpreadable(str)); // false
